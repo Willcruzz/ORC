64449eae5d3845304d593fb95ad3ec8007a2fb05
Guilherme <guilhermecruzdepaula2@gmail.com>
2024-12-10 17:40:59 -0300

    Add CORS policy package to Flask API

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Configuração da API do Claude
 CLAUDE_API_URL = "https://api.anthropic.com/v1/messages"
 CLAUDE_API_KEY = ""
 
 @app.route('/processar', methods=['POST'])
+@cross_origin(origins='*')
 def processar_com_claude():
     data = request.json
     texto_entrada = data.get('texto')
 
     headers = {
-        'Authorization': f'Bearer {CLAUDE_API_KEY}',
+        'x-api-key': f'{CLAUDE_API_KEY}',
+        'anthropic-version': '2023-06-01',
         'Content-Type': 'application/json',
     }
     payload = {
@@ -23,6 +28,10 @@ def processar_com_claude():
     try:
         response = requests.post(CLAUDE_API_URL, headers=headers, json=payload)
         response_data = response.json()
+
+        if response_data.get('type') == 'error':
+            return jsonify({"resultado": response_data.get('error').get('message')})
+
         return jsonify({"resultado": response_data.get('output', 'Sem resposta')})
     except Exception as e:
         return jsonify({"erro": str(e)}), 500
